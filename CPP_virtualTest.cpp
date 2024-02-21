c4979269e2899faf74a4aee8f44fa414d1982e7c
Alireza <aliry95>
2024-02-21 09:40:41 +0100

    CCP_virtualTest updated

--- CPP_virtualTest.cpp
+++ CPP_virtualTest.cpp
@@ -1,34 +1,73 @@
+/* README ----------------------------------------------------------------------
+Compile with "g++ CPP_virtualTest.cpp -o virtualTest".
+Run with "./virtualTest 1" for the Spherical particles
+and      "./virtualTest 2" for the Box-shaped particles
+----------------------------------------------------------------------------- */
 #include <iostream>
+#include <new>
 using namespace std;
 
 class Particle {
     public:
-        virtual void print() const { cout << "Particle Function" << endl; }
+        virtual double computeVolume() const 
+        { 
+            cout << "computeVolume() is not defined for this type" << endl; 
+            return( 0. );
+        }
 };
 
 class Sphere : public Particle {
+    private:
+        double m_radius;
     public:
-        void print() const { cout << "Sphere Function" << endl; }
+        Sphere( double rad )
+        : m_radius( rad )
+        {}
+
+        double computeVolume() const 
+        { 
+            return( 3.1415 * m_radius * m_radius );
+        };
 };
 
 class Box : public Particle {
+    private:
+        double m_extent;
     public:
-        void print() const { cout << "Box Function" << endl; }
+        Box( double extent )
+        : m_extent( extent )
+        {}
+
+        double computeVolume() const
+        { 
+            return( m_extent * m_extent * m_extent );
+        }
 };
 
-void printClass( const Particle& Particle )
+void multiplyVolumeByTwo( Particle const& Particle )
 {
-    Particle.print();
+    double tmp = 2. * Particle.computeVolume();
+    cout << "Volume multiplied by two is " << tmp << "\n";
 };
 
-int main() {
-    // pointer of Particle type that points to a Sphere or Box obj
-    Particle* Particle1 = new Sphere();
-    Particle* Particle2 = new Box();
+int main( int argc, char* argv[] ) 
+{
+    // BuilderFactory
+    Particle* myParticle;
+    switch ( stoi( argv[1] ) )
+    {
+    case ( 1 ):
+        myParticle = new Sphere( 1. );
+        break;
+    case ( 2 ):
+        myParticle = new Box( 1. );
+        break;
+    default:
+        break;
+    }
 
     // calls member function of Derived class
-    printClass( *Particle1 );
-    printClass( *Particle2 );
+    multiplyVolumeByTwo( *myParticle );
 
     return 0;
 }
